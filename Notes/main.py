73cd4b7261df0373707a4769e919ccdac4fb6b75
Milli Sood <dxnut2359@gmail.com>
2026-06-09 20:15:30 +0530

    Add files via upload

---
+++ Notes/main.py
@@ -0,0 +1,7 @@
+# My First Python Program
+# Comments: They are a way to add notes to our program code for ourselves or for other people.
+#           Comments are not displayed on the output terminal
+
+print("Hello World!")
+print("-------------")
+print("5th March, 2025")
