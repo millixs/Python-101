e26e69bda88796999adf668ef531d84e84ddb355
Milli Sood <dxnut2359@gmail.com>
2026-06-09 20:00:44 +0530

    Add files via upload

---
+++ Questions/1.py
@@ -0,0 +1,8 @@
+# Program to calculate the area of a rectangle using user input
+
+length = float(input("Enter the length: "))
+breadth = float(input("Enter the breadth: "))
+
+area = length * breadth
+
+print(f"Area: {area} cm²")
---
+++ Questions/2.py
@@ -0,0 +1,10 @@
+# Program to create a shopping bill by taking item details from the user
+
+item = input("What item would you like to purchase?: ")
+price = float(input("What is the price?: "))
+quantity = int(input("How many would you like?: "))
+
+total = price * quantity
+
+print(f"You have bought {quantity} x {item}/s")
+print(f"Your total is: ${total}")
